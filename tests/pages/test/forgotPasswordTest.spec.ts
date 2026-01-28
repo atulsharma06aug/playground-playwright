151716c4f44df5a494e2349bf6ed9154dd7e2c29
Atul Sharma <atulsharma06aug@gmail.com>
2026-01-28 12:41:12 +0530

    POM Update

--- tests/pages/test/forgotPasswordTest.spec.ts
+++ tests/pages/test/forgotPasswordTest.spec.ts
@@ -1,6 +1,17 @@
 import { expect, test } from '../../fixtures/fixtures'
 
 
+const $userProfile = {
+    "username" : process.env.USER_EMAIL
+}
+
+test("TC_FP_01 Open Forgot Password Page", {tag: '@forgotPassword'}, 
+    async ({page, ForgotPasswordPage} )=>{
+        console.log("going to forgot password page")
+        await ForgotPasswordPage.gotoForgotUserPage()
+    }
+)
+
 test("TC_FP_001 @forgotPassword", { tag: '@forgotPassword'},
     async ({ page, LoginPageObject, ForgotPasswordPage }) => {
         console.log("inside")
@@ -12,6 +23,6 @@ test("TC_FP_001 @forgotPassword", { tag: '@forgotPassword'},
 test("TC-02, Forgot password", { tag: '@forgotPassword' }, async ({ page, ForgotPasswordPage }) => {
     console.log("test page")
     await ForgotPasswordPage.gotoForgotUserPage()
-    await ForgotPasswordPage.enterValidUserEmailID(process.env.USER_EMAIL!)
+    await ForgotPasswordPage.enterValidUserEmailID($userProfile.username!)
     await ForgotPasswordPage.clickSendResetLinkButton()
 })
